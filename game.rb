bbe072ccb090bd5c2ef8f3cfa1448433bc0a8200
App Academy Student <student@appacademy.io>
2013-09-26 11:10:01 -0700

    add slide moves and jump moves. Need to refactor

--- game.rb
+++ game.rb
@@ -9,28 +9,83 @@ Board Class
   illegal move should raise Invalid move error
 
 Piece Class
-  # slide_moves
-  # jumps_moves
+  # slide_moves - lists possible moves
+  # jumps_moves - lists possible moves
 Game Class
 
 =end
-
+require 'colorize'
 class Piece
-  attr_accessor :color, :king
+  MOVES_WHITE = [[-1, -1], [-1, 1]]
+  MOVES_RED = [[1, -1], [1, 1]]
+  attr_accessor :color, :pos, :king, :symbol
+  attr_reader :board
 
-  def initialize(color, pos)
+  def initialize(color, pos, board)
     @color = color
     @pos = pos
     @king = false
-    @symbol = @color == :red ? "26C0" : "26C2"
+    @symbol = "\u26C2"
+    @board = board
+  end
+
+  def slide_moves
+    possible_moves = []
+    move_directions = []
+    move_directions.concat(MOVES_WHITE) if color == :white || king == true
+    move_directions.concat(MOVES_RED) if color == :red || king == true
+    move_directions.each do |direction|
+      location = [pos[0] + direction[0], pos[1] + direction[1]]
+      possible_moves << location if self.board.valid_move?(location)
+    end
+    possible_moves
+  end
+
+  def jumps_moves
+    possible_moves = []
+    move_directions = []
+    move_directions.concat(MOVES_WHITE) if color == :white || king == true
+    move_directions.concat(MOVES_RED) if color == :red || king == true
+    #dry this out
+    move_directions.each do |direction|
+      jump_location = [pos[0] + 2 * direction[0], pos[1] + 2 * direction[1]]
+      middle_location = [pos[0] + direction[0], pos[1] + direction[1]]
+      middle_piece = self.board.piece_at_location(middle_location)
+      if self.board.valid_move?(jump_location) && middle_piece && middle_piece.color != self.color
+        possible_moves << jump_location
+      end
+
+    end
+    possible_moves
   end
 
 end
 
 class Board
-
+  attr_accessor :pieces
   def initialize
-    @game_board = build_board
+    @pieces = build_board
+  end
+
+  def valid_move?(pos)
+    return false if !pos[0].between?(0,7) && !pos[1].between?(0,7)
+    return false if all_locations.include?(pos)
+    true
+  end
+
+  def piece_at_location(location)
+    pieces.select {|piece| piece.pos == location}[0]
+
+  end
+
+  def all_locations
+    pieces.map {|piece| piece.pos}
+  end
+
+  def perform_slide
+  end
+
+  def perform_jump
   end
 
   def build_board
@@ -56,16 +111,37 @@ class Board
     black = results.sort[12..-1]
 
     red_pieces = red.map do |pos|
-      Piece.new(:red, pos)
+      Piece.new(:red, pos, self)
     end
     black_pieces = black.map do |pos|
-      Piece.new(:black, pos)
+      Piece.new(:white, pos, self)
     end
     red_pieces + black_pieces
   end
 
   def render
+    print_array = Array.new(8) { Array.new(8) }
 
+    @pieces.each do |piece|
+      print_array[piece.pos[0]][piece.pos[1]] = piece.symbol.colorize(piece.color)
+    end
+
+    print "     "
+    puts [0, 1, 2, 3, 4, 5, 6, 7].join("    ")
+
+    print_array.each_with_index do |row, index|
+      print "#{index}  "
+      row.each do |tile|
+        if tile.nil?
+          print "|    " if tile.nil?
+        else
+          print "| #{tile}  "
+        end
+      end
+      puts "|"
+      puts "   -----------------------------------------"
+    end
+    nil
   end
 end
 
